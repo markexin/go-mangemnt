61aeace27a80500914196ba513e62dcff03648ea
maqun <maqun1@xiaomi.com>
2022-10-10 10:34:37 +0800

    feat: 增加网关前缀

--- main.go
+++ main.go
@@ -6,14 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var PREFIX string = "/api"
+
 func main() {
 
 	// 1.创建路由
 	r := gin.Default()
 	// 2.绑定路由规则，执行的函数
 	// gin.Context，封装了request和response
-	r.POST("/login", routes.Login)
-	r.GET("/products", routes.GetProducts)
+	r.POST(PREFIX+"/login", routes.Login)
+	r.GET(PREFIX+"/products", routes.GetProducts)
 	// 3.监听端口，默认在8080
 	// Run("里面不指定端口号默认为8080")
 	r.Run(":8001")
